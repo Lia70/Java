f3550d60ac211181f32990e58eeb7329e4d3d50a
Алина Исманова <alinaismanova@MacBook-Air-Alina.local>
2023-11-30 12:35:41 +0300

    ДЗ 6 по Java

---
+++ Laptop.java
@@ -0,0 +1,22 @@
+
+public class Laptop {
+
+    public Laptop(String string, int i, int j, String string2, String string3, double d) {
+    }
+    public Object name;
+    public int storageCap;
+    public int ram;
+    public Object os;
+    public Object color;
+    public double diagonal;
+    public int getRam() {
+        return 0;
+    }
+    public int getStorageCap() {
+        return 0;
+    }
+    public double getDiagonal() {
+        return 0;
+    }
+
+}
